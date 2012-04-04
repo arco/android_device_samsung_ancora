2642718995f01d8a22a75ad6e304ad1a7d59d3a0
Arne Coucheron <arco68@gmail.com>
2012-04-05 00:57:41 +0200

    Add e2fsprogs utilities and hcitool

--- ancora.mk
+++ ancora.mk
@@ -122,8 +122,17 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_PACKAGES += \
+    badblocks \
+    e2fsck \
+    e2label \
+    mke2fs \
+    resize2fs\
+    tune2fs \
     make_ext4fs \
     setup_fs
+
+PRODUCT_PACKAGES += \
+    hcitool
 
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
