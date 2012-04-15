89701aee4a7eec12b9399269514f775f17c2029f
Arne Coucheron <arco68@gmail.com>
2012-04-15 02:28:02 +0200

    Change inherit ordering in cm.mk

--- cm.mk
+++ cm.mk
@@ -1,11 +1,13 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
 
 PRODUCT_RELEASE_NAME := ancora
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Setup device configuration
 PRODUCT_NAME := cm_ancora
