1b543d7193b360470de77ab75f560a65a6ab9e3b
Arne Coucheron <arco68@gmail.com>
2012-05-19 05:08:33 +0200

    Include CIFS module

--- ancora.mk
+++ ancora.mk
@@ -78,6 +78,7 @@ PRODUCT_COPY_FILES += \
 # Modules
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/bthid.ko:root/lib/modules/bthid.ko \
+    $(LOCAL_PATH)/prebuilt/cifs.ko:root/lib/modules/cifs.ko \
     $(LOCAL_PATH)/prebuilt/dhd.ko:root/lib/modules/dhd.ko
 
 # LPM
