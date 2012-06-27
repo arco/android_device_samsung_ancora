4757ff38b731b46431a7f219c15106292b0243b2
Arne Coucheron <arco68@gmail.com>
2012-06-27 08:20:56 +0200

    Change wifi config according to driver revert in kernel

--- ancora.mk
+++ ancora.mk
@@ -163,4 +163,3 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/samsung/ancora/device-vendor.mk)
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
