cc6a7fdc364df916cb4ae7646510ef534614de17
Arne Coucheron <arco68@gmail.com>
2012-11-30 00:06:37 +0100

    Build liblights from source

--- ancora.mk
+++ ancora.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
     gralloc.msm7x30 \
     hwcomposer.msm7x30 \
     gps.msm7x30 \
+    lights.msm7x30 \
     audio.primary.msm7x30 \
     audio_policy.msm7x30 \
     audio_policy.conf \
