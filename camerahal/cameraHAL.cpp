7c14d6744e48bede7f1600677dd600c9e883c01d
Arne Coucheron <arco68@gmail.com>
2013-02-02 23:46:47 +0100

    Fix frame rate for front camera

--- camerahal/cameraHAL.cpp
+++ camerahal/cameraHAL.cpp
@@ -407,6 +407,7 @@ void CameraHAL_FixupParams(android::CameraParameters &camParams, priv_camera_dev
 
     if (dev->cameraid == CAMERA_ID_FRONT) {
         camParams.set(CameraParameters::KEY_SUPPORTED_ISO_MODES, "");
+        camParams.set(CameraParameters::KEY_SUPPORTED_PREVIEW_FRAME_RATES, "15");
     }
 
     if (dev->cameraid == CAMERA_ID_BACK) {
@@ -419,6 +420,8 @@ void CameraHAL_FixupParams(android::CameraParameters &camParams, priv_camera_dev
         camParams.set(CameraParameters::KEY_MAX_ZOOM, "8");
         camParams.set(CameraParameters::KEY_ZOOM_RATIOS, "100,125,150,175,200,225,250,275,300");
         camParams.set(CameraParameters::KEY_ZOOM_SUPPORTED, CameraParameters::TRUE);
+
+        camParams.set(CameraParameters::KEY_SUPPORTED_PREVIEW_FRAME_RATES, "30");
     }
 
     camParams.set(CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, 4);
