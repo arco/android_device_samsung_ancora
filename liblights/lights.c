2ba142086d3d3beb24508c3b64c76b825a6d1f8d
blackcat67 <dogukanbey42@gmail.com>
2013-06-11 15:52:49 +0300

    Update lights.c

--- liblights/lights.c
+++ liblights/lights.c
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <hardware/lights.h>
 
-#define BACKLIGHT_ON    	0x1
+#define BACKLIGHT_ON		0x1
 #define BACKLIGHT_OFF		0x2
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
