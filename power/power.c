65df51c2bc35e70eea3b3ec0ad002669d9b89e76
Arne Coucheron <arco68@gmail.com>
2013-07-23 05:49:32 +0200

    power: Manage io_is_busy

--- power/power.c
+++ power/power.c
@@ -132,6 +132,7 @@ static void configure_governor()
 
     } else if (strncmp(governor, "interactive", 11) == 0) {
         sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time", "90000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
         sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq", "1024000");
         sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay", "30000");
     }
