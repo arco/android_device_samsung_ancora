dbd4cd9d1115f60c0e3b9161039204c8fa716777
Arne Coucheron <arco68@gmail.com>
2014-02-28 12:03:55 +0100

    Remove inherit of gsm.mk
     * GSM and CDMA are consolidated now

--- cm.mk
+++ cm.mk
@@ -1,6 +1,3 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
