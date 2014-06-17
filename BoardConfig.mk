7f1a27aab8b5a0dc6eed35b418e52d0d7f87d2a6
Arne Coucheron <arco68@gmail.com>
2014-06-17 02:03:50 +0200

    Enable NO_SECURE_DISCARD for recovery
     * To speed up formatting after we enabled MMC_CAP_ERASE in kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora/recovery/recovery_keys.c
 TARGET_RECOVERY_INITRC := device/samsung/ancora/config/init.recovery.rc
 TARGET_RECOVERY_FSTAB := device/samsung/ancora/config/fstab.qcom
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 # End recovery stuff
 
 TARGET_KERNEL_SOURCE := kernel/samsung/msm7x30-common
