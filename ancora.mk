a900a4908ef6888d7262487bd6683d3d0b31fcb9
Arne Coucheron <arco68@gmail.com>
2014-12-17 06:10:30 +0100

    Add missing backslash

--- ancora.mk
+++ ancora.mk
@@ -171,7 +171,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
-    ro.adb.secure=0
+    ro.adb.secure=0 \
     ro.debuggable=1 \
     persist.service.adb.enable=1
 
