AriesVE-DevCon-TEAM/android_device_samsung_ariesve

gps: Fork our own version of the GPS HAL

BoardConfig.mk
@@ -90,6 +90,7 @@ BOARD_USES_LEGACY_RIL := true
 BOARD_RIL_CLASS := ../../../device/samsung/ariesve/ril/
 
 # GPS
+TARGET_GPS_HAL_PATH := device/samsung/ariesve/gps
 BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x30
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000

arco/android_device_samsung_ancora

BoardConfig.mk
@@ -122,6 +122,7 @@ TARGET_PROVIDES_LIBLIGHT := true
 TARGET_PROVIDES_POWERHAL := true
 
 # GPS
+TARGET_GPS_HAL_PATH := device/samsung/ancora/gps
 BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x30