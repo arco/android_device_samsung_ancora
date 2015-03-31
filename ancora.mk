7679929133a55152033dcc9070a360dee304a00f
Michael Bestas <mikeioannina@gmail.com>
2015-04-01 01:59:56 +0300

    device: Use ART small mode
    https://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode

--- ancora.mk
+++ ancora.mk
@@ -163,7 +163,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.image-dex2oat-filter=speed
 
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
