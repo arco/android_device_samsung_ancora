d747d8eab3f8d778e5ab35eb3471b84605723f29
khusika <khusikadhamar@gmail.com>
2015-04-04 12:52:46 +0700

    Add support for TWRP

--- ancora.mk
+++ ancora.mk
@@ -60,7 +60,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
-    $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom
+    $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
 
 # Wifi calibration
 PRODUCT_COPY_FILES += \
