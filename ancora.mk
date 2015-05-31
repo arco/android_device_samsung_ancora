93a012f6a9e5c651ee626d5398729c6f7e230b4d
Michael Bestas <mikeioannina@gmail.com>
2015-05-31 17:46:31 +0300

    device: Set dalvik.vm.dex2oat-filter=balanced
    * ~11x runtime performance improvement

--- ancora.mk
+++ ancora.mk
@@ -163,7 +163,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.dex2oat-filter=balanced \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed
 
