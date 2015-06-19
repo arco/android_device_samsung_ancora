b2ef900598c6b7cfdd689166a051fdb46d8fc47e
Steve Kondik <steve@cyngn.com>
2015-06-19 23:59:00 +0200

    device: Don't copy FFMPEG config anymore
    Change-Id: I5f7b5082c1f823247c4bd7d7cc9b45004d9b216b

--- ancora.mk
+++ ancora.mk
@@ -47,7 +47,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
